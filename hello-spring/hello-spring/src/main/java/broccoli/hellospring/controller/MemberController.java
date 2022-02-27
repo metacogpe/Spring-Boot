d00a2972e6026fb139e7a1c83c38c4145f24e977
metacogpe <kakao.com>
2022-02-27 09:23:34 +0900

    memberList

--- hello-spring/hello-spring/src/main/java/broccoli/hellospring/controller/MemberController.java
+++ hello-spring/hello-spring/src/main/java/broccoli/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import broccoli.hellospring.domain.Member;
 import broccoli.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 
 @Controller
@@ -31,6 +34,13 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
     }
 
 
