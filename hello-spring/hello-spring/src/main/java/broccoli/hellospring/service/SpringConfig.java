3f8682dc0badd7a9ad45a8c9e860863fb37a31a3
metacogpe <kakao.com>
2022-03-01 21:18:19 +0900

    Starting Spring Boot and JPA

--- hello-spring/hello-spring/src/main/java/broccoli/hellospring/service/SpringConfig.java
+++ hello-spring/hello-spring/src/main/java/broccoli/hellospring/service/SpringConfig.java
@@ -2,22 +2,25 @@ package broccoli.hellospring.service;
 
 import broccoli.hellospring.repository.JdbcMemberRepository;
 import broccoli.hellospring.repository.JdbcTemplateMemberRepository;
+import broccoli.hellospring.repository.JpaMemberRepository;
 import broccoli.hellospring.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+//    private DataSource dataSource;
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+    private EntityManager em;  // jpa를 위해 위의 dataSource 를 remark 하고 EntityManager 사용
 
     @Bean // 스프링빈에 memberService 를 등록
     public MemberService memberService() {
@@ -28,6 +31,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();   // 기존 방식 : DB 미연결
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
