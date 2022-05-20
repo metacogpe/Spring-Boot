55fd475cfe4bf6a751666c03be50d04a0646c853
metacog <metacog@kakao.com>
2022-05-20 16:49:28 +0900

    IoC : ingredientFactory for chef

--- helloproject/src/test/java/com/example/helloproject/ioc/ChefTest.java
+++ helloproject/src/test/java/com/example/helloproject/ioc/ChefTest.java
@@ -1,17 +1,24 @@
 package com.example.helloproject.ioc;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class ChefTest {
+
+    @Autowired  // Spring Boot IoC컨테이너에서 가져오기
+    IngredientFactory ingredientFactory;
 
     @Test
     void 돈가스_요리하기() {
         // 준비
-        // 재료 공장으로부터 재료를 조달받기
-        IngredientFactory ingredientFactory = new IngredientFactory();
-        Chef chef = new Chef(ingredientFactory); // chef가 재료 공장에 대한 정보를 DI(동작에 필요한 객체를 외부에서 받아옴)
+//        Chef chef = new Chef();
+        // 위의 코드를 외부에서 식재료를 주입하는 DI로 변경
+//        IngredientFactory ingredientFactory = new IngredientFactory();
+        Chef chef = new Chef(ingredientFactory);
         String menu = "돈가스";
 
         // 수행
@@ -30,7 +37,7 @@ class ChefTest {
         // 준비
 //        Chef chef = new Chef();
         // 위의 코드를 외부에서 식재료를 주입하는 DI로 변경
-        IngredientFactory ingredientFactory = new IngredientFactory();
+//        IngredientFactory ingredientFactory = new IngredientFactory();
         Chef chef = new Chef(ingredientFactory);
         String menu = "스테이크";
 
@@ -48,7 +55,7 @@ class ChefTest {
     @Test
     void 크리스피_치킨_요리하기() {
         // 준비
-        IngredientFactory ingredientFactory = new IngredientFactory();  // 공장을 만들고,
+//        IngredientFactory ingredientFactory = new IngredientFactory();  // 공장을 만들고,
         Chef chef = new Chef(ingredientFactory);                        // 세프에게 재료 공급
         String menu = "크리스피 치킨";
 
